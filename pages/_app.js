79022851fab80ea205329bc60ed9aec8e05068cd
ankit-techindustan <ankitjoshi.techindustan@gmail.com>
2020-07-10 14:36:45 +0530

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../styles/index.scss';
+import { appWithTranslation } from '../i18n';
+// import App from 'next/app';
+import store from '../redux/store'
+
+// class AppContainer extends App {
+
+//   static async getInitialProps({ Component, ctx }) {
+//     const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
+//     return { pageProps }
+//   }
+
+//   render() {
+//     const { Component, pageProps } = this.props;
+//     return <Component {...pageProps} />
+//   }
+// }
+
+function AppContainer({ Component, pageProps }) {
+  return (
+    <Component {...pageProps} />
+  )
+}
+
+AppContainer.getInitialProps = async ({ Component, ctx }) => {
+  const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
+  return {
+    pageProps
+  }
+}
+
+export default store.withRedux(appWithTranslation(AppContainer));
---
+++ pages/auth/login.js
@@ -0,0 +1,36 @@
+import Head from 'next/head';
+import React, { useState, useEffect } from 'react';
+import Layout from '../../components/layout';
+import { Link } from '../../i18n';
+import { useSelector } from 'react-redux';
+import { useTranslation } from 'react-i18next';
+
+function Login() {
+  const state = useSelector(state => state);
+  const { t, i18n } = useTranslation();
+
+  return (
+    <Layout>
+      <div className="container">
+        <Head>
+          <title>Truckbook - Login</title>
+          <link rel="icon" href="/images/favicon.png" />
+        </Head>
+        <main>
+          <h1>{t('login')}</h1>
+          <h2>{t('This is Ankit Joshi i want to tell this')}</h2>
+          <Link href="/auth/signup"><a>{t('signup')}</a></Link><br />
+          <Link href="/auth/forgot-password"><a>{t('Forgot Password?')}</a></Link>
+        </main>
+      </div>
+    </Layout>
+  )
+}
+
+Login.getInitialProps = async () => {
+  return {
+    namespacesRequired: ['common']
+  }
+};
+
+export default Login;
